8e83720886eb93655fc56e1412369a89905ebe6f
fwup <your.email@example.com>
2025-01-02 16:19:31 +0000

    Update project configuration and file paths for improved structure and deployment
    - Updated the favicon and script source paths in index.html for correct absolute linking.
    - Introduced new TypeScript build information files (tsconfig.node.tsbuildinfo and tsconfig.tsbuildinfo) for better type management.
    - Added a new Vite configuration file (vite.config.js) to define project settings and build options.
    - Refined output configuration in vite.config.ts to specify hashed file names for better asset management.
    - Enhanced the overall project structure to support deployment and maintainability.

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'node:url';
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: '/smolmart_prototype/',
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        rollupOptions: {
+            output: {
+                entryFileNames: "assets/[name].[hash].js",
+                chunkFileNames: "assets/[name].[hash].js",
+                assetFileNames: "assets/[name].[hash].[ext]"
+            }
+        }
+    }
+});
