9c7aa2a3f6c86f2e3aa0c608024a5734ec18f984
fwup <your.email@example.com>
2025-01-02 16:21:32 +0000

    Update favicon path in index.html and adjust Vite base configuration for improved deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/smolmart_prototype/',
+  base: process.env.NODE_ENV === 'production' ? '/smolmart_prototype/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
